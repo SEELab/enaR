e8fcbe125ec70fe0c60d6a0c9765eb39560bbf6d
mklau <matthewklau@fas.harvard.edu>
2016-01-20 16:22:08 -0500

    added first pass at enaPlot

---
+++ R/enaPlot.R
@@ -0,0 +1,18 @@
+#' enaPlot  --- generate a network plot
+#' INPUT = network object
+#' OUTPUT = network plot
+#' M.K. Lau Jan 2016
+#' ---------------------------------------------------
+
+library(ggnet)
+
+enaPlot <- function(x){
+
+ x %v% 'alp.storage' <- x %v% 'storage' / max(x %v% 'storage')
+ x %v% 'col.living' <- ifelse(x %v% 'living' == 'FALSE','red','grey25')
+ x %v% 'vertex.names' <- sapply(x %v% 'vertex.names',function(s) 
+     paste(sapply(strsplit(s,' ')[[1]],substr,start=1,stop=2),collapse='')
+                                )
+ ggnet2(x,edge.size='flow',node.color='col.living',node.size='storage',label=TRUE,label.size=6,label.alpha=0.75) + guides(color=FALSE)
+ 
+}
