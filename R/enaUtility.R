1469ec15d67d32881d024bb89ca1411ed05be153
borretts <stuart.borrett@gmail.com>
2016-02-26 22:00:31 -0500

    change enaUtility check for storage

--- R/enaUtility.R
+++ R/enaUtility.R
@@ -9,7 +9,7 @@ enaUtility <- function(x, type=c('flow','storage'),
                        eigen.check=TRUE,
                        balance.override=FALSE,tol=10){
                                         #Missing Data Check
-    if (any(is.na(x%v%'storage'))){
+    if (type == 'storage' && any(is.na(x%v%'storage'))){
         warning('This function requires quantified storage values.')
     }else{
         orient <- get.orient()
