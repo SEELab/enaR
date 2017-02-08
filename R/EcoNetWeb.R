60043ca89fa033ff6fa4e48a658c48b7902f36bc
mklau <matthewklau@fas.harvard.edu>
2017-02-08 11:28:47 -0500

    adds econet functions

---
+++ R/EcoNetWeb.R
@@ -0,0 +1,47 @@
+### Reading models from the econet website
+### MKLau 31 Mar 2016
+
+
+
+
+
+
+
+#' Access example EcoNet models from the EcoNet website.
+#' 
+#' This function allows the user to access models that are presented on the
+#' website for EcoNet, the web-based interface for conducting ENA
+#' (http://eco.engr.uga.edu/), by Caner Kazanci at the University of Georgia.
+#' 
+#' 
+#' @param x The URL for the EcoNet examples.
+#' @param model.name The model to be accessed. If 'prompt' the user will be
+#' asked for the model they wish to use. Can also be a number for the model or
+#' the name of the model.
+#' @return Returns the model formatted as a network object.
+#' @author Matthew K. Lau
+#' @seealso \code{\link{read.EcoNet}}
+#' @references Kazanci, C., 2007. EcoNet: A new software for ecological
+#' modeling, simulation and network analysis, Ecol. Model., Vol 208/1 pp 3-8.
+#' @export EcoNetWeb
+EcoNetWeb <- function(x='http://eco.engr.uga.edu/Examples/examples.html',model.name='prompt'){
+    x <- readLines(x)
+    x <- x[1:(grep('<!-- Copy paste for new model',x)-1)]
+    mod.names <- x[grep('<h2>',x)]
+    mod.names <- sub('<h2>','',mod.names);mod.names <- sub('</h2>','',mod.names)
+    mod.names <- strsplit(mod.names,split=' ')
+    mod.names <- lapply(mod.names,function(x) x[x != ''])
+    mod.names <- unlist(lapply(mod.names,paste,collapse=' '))
+    mod.loc <- cbind(grep('<hr class=\"duz\">',x),grep('</pre></div></td></tr></table>',x))
+    mods <- apply(mod.loc,1,function(loc,x) x[loc[1]:loc[2]],x=x)
+    if (model.name == 'prompt'){
+        print(mod.names)
+        model.name <- as.numeric(readline('Enter model number:'))
+        out <- read.EcoNet(mods[[model.name]])
+    }else if (is.numeric(model.name[1])){
+        out <- read.EcoNet(mods[[model.name[1]]])
+    }else{
+        out <- read.EcoNet(mods[[agrep(model.name[1],mod.names,ignore.case=T)]])
+    }
+    return(out)
+}
---
+++ R/write.EcoNet.R
@@ -0,0 +1,60 @@
+###Function to output a model for EcoNet
+###http://eco.engr.uga.edu/DOC/econet2.html
+###MKLau 17nov2014
+
+
+
+
+
+
+
+#' Write enaR models to an EcoNet formatted file.
+#' 
+#' Creates an EcoNet model from an enaR network object that can be used with
+#' the online interface for EcoNet.
+#' 
+#' 
+#' @param x Network object.
+#' @param file The file name or path. If a simple file name is given, this
+#' function uses the current working directory by default.
+#' @param mn The model name that EcoNet will use. The DEFAULT is 'ena_model'.
+#' @param zero.flows LOGICAL: should zero flow values be written?
+#' @return An EcoNet formatted text file is created from the model, which can
+#' be input at http://eco.engr.uga.edu.
+#' @author Matthew K. Lau
+#' @references About EcoNet (http://eco.engr.uga.edu/DOC/econet1.html)
+#' 
+#' Kazanci, C. 2009. Handbook of Ecological Modelling and Informatics, by WIT
+#' Press.
+#' @export write.EcoNet
+write.EcoNet <- function(x='model',file='file path',mn='ena_model',zero.flows=FALSE){
+    x <- unpack(x)
+###node names
+    nn <- rownames(x$F)
+    nn <- strsplit(nn,split='')
+    nn <- lapply(nn,function(x) x[x%in%letters|x%in%LETTERS|x%in%(1:9)])
+    nn <- unlist(lapply(nn,paste,collapse=''))
+    rownames(x$F) <- colnames(x$F) <- nn
+###Write model name
+    write.table(paste('###',mn),file=file,col.names=FALSE,row.names=FALSE,quote=FALSE)
+###initial conditions
+    write.table(paste('###','initial conditions'),file=file,col.names=FALSE,row.names=FALSE,quote=FALSE,append=TRUE)
+    write.table(paste(rownames(x$F),x$X,sep='='),file=file,col.names=FALSE,row.names=FALSE,quote=FALSE,append=TRUE)
+###write inputs
+    write.table(paste('###','inputs'),file=file,col.names=FALSE,row.names=FALSE,quote=FALSE,append=TRUE)
+    write.table(paste('*',' -> ',rownames(x$F),' c=',x$z,sep=''),file=file,col.names=FALSE,row.names=FALSE,quote=FALSE,append=TRUE)
+###write flows
+    write.table(paste('###','flows'),file=file,col.names=FALSE,row.names=FALSE,quote=FALSE,append=TRUE)
+    for (i in 1:nrow(x$F)){
+        for (j in 1:nrow(x$F)){
+            if (x$F[i,j] == 0){}else{
+                write.table(paste(rownames(x$F)[i],' -> ',colnames(x$F)[j],' c=',x$F[i,j],sep=''),
+                            file=file,col.names=FALSE,row.names=FALSE,
+                            quote=FALSE,append=TRUE)
+            }
+        }
+    }
+###write outputs
+    write.table(paste('###','outputs'),file=file,col.names=FALSE,row.names=FALSE,quote=FALSE,append=TRUE)
+    write.table(paste(rownames(x$F),' -> ','*',' c=',(x$r+x$e),sep=''),file=file,col.names=FALSE,row.names=FALSE,quote=FALSE,append=TRUE)
+}
